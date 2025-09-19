352b7330b2ff2e9a685d4bf321ac4f78410783a5
scheshan <5373827@qq.com>
2025-09-19 20:30:06 +0800

    lesson 1 part 2,3

--- src/bin/kvs.rs
+++ src/bin/kvs.rs
@@ -1,3 +1,25 @@
-fn main() {
+use std::process::exit;
+use clap::{Parser, Subcommand};
 
+#[derive(Parser)]
+#[command(version)]
+struct Args {
+
+    #[command(subcommand)]
+    cmd: Command
+}
+
+#[derive(Subcommand)]
+enum Command {
+    Set {key: String, value: String},
+    Get {key: String},
+
+    #[command(name = "rm")]
+    Remove {key: String}
+}
+
+fn main() {
+    let args = Args::parse();
+    eprint!("unimplemented");
+    exit(-1);
 }
