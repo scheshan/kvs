433e9c45c8a5f9643ce93326121954b9de6702c1
scheshan <5373827@qq.com>
2025-09-19 20:52:27 +0800

    lesson 1 part 4,5,6

--- src/lib.rs
+++ src/lib.rs
@@ -1,22 +1,25 @@
+use std::collections::HashMap;
 
 pub struct KvStore {
-
+    hm: HashMap<String, String>
 }
 
 impl KvStore {
     pub fn new() -> Self {
-        Self{}
+        Self{
+            hm: HashMap::new()
+        }
     }
 
     pub fn set(&mut self, key: String, value: String) {
-
+        self.hm.insert(key, value);
     }
 
     pub fn get(&self, key: String) -> Option<String> {
-        return None
+        self.hm.get(&key).map(|str| str.clone())
     }
 
     pub fn remove(&mut self, key: String) {
-        
+        self.hm.remove(&key);
     }
 }
