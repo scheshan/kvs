e82da40600eee917165f0dcacf0199534ef1132b
scheshan <5373827@qq.com>
2025-09-19 17:50:32 +0800

    lesson 1 part 1

---
+++ src/lib.rs
@@ -0,0 +1,22 @@
+
+pub struct KvStore {
+
+}
+
+impl KvStore {
+    pub fn new() -> Self {
+        Self{}
+    }
+
+    pub fn set(&mut self, key: String, value: String) {
+
+    }
+
+    pub fn get(&self, key: String) -> Option<String> {
+        return None
+    }
+
+    pub fn remove(&mut self, key: String) {
+        
+    }
+}
