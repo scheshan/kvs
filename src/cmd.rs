74d40a0bc1351d2891915af7e6a19cc8d745d7fb
scheshan <5373827@qq.com>
2025-10-08 09:53:51 +0800

    add log logic

--- src/cmd.rs
+++ src/cmd.rs
@@ -8,8 +8,7 @@ impl Command {
     pub fn to_bytes(&self) -> Vec<u8> {
         let len = self.data_len();
 
-        let mut buf = Vec::<u8>::with_capacity(len + 9);
-        Self::write_u64(&mut buf, len as u64);
+        let mut buf = Vec::<u8>::with_capacity(len + 1);
 
         match self {
             Command::Set(key, value) => {
@@ -30,6 +29,30 @@ impl Command {
         buf
     }
 
+    pub fn from_bytes(buf: &Vec<u8>) -> Self {
+        let typ = buf[0];
+        let buf = &buf[1..];
+
+        match typ {
+            1 => {
+                let (key, buf) = Self::read_string(&buf);
+                let (value, buf) = Self::read_string(&buf);
+                Self::Set(key, value)
+            }
+            2 => {
+                let (key, buf) = Self::read_string(&buf);
+                Self::Get(key)
+            }
+            3 => {
+                let (key, buf) = Self::read_string(&buf);
+                Self::Remove(key)
+            }
+            _ => {
+                panic!("invalid format")
+            }
+        }
+    }
+
     fn data_len(&self) -> usize {
         match self {
             Command::Set(key, value) => 8 + key.as_bytes().len() + 8 + value.as_bytes().len(),
@@ -45,5 +68,59 @@ impl Command {
     fn write_string(v: &mut Vec<u8>, str: &String) {
         Self::write_u64(v, str.len() as u64);
         v.extend_from_slice(str.as_bytes());
+    }
+
+    fn read_u64(buf: &[u8]) -> (u64, &[u8]) {
+        let (data_buf, rest) = buf.split_at(8);
+        let len = u64::from_be_bytes(data_buf.try_into().unwrap());
+        (len, rest)
+    }
+
+    fn read_string(buf: &[u8]) -> (String, &[u8]) {
+        let (len, rest) = Self::read_u64(buf);
+        let (data_buf, rest) = rest.split_at(len as usize);
+        let str = String::from_utf8_lossy(data_buf).to_string();
+        (str, rest)
+    }
+}
+
+#[test]
+fn command_serialize_and_deserialize() {
+    let mut cmd = Command::Set("hello".to_string(), "world".to_string());
+    let mut buf = cmd.to_bytes();
+    cmd = Command::from_bytes(&buf);
+
+    match cmd {
+        Command::Set(k, v) => {
+            assert_eq!(k, "hello");
+            assert_eq!(v, "world");
+        }
+        _ => {
+            panic!("test failed")
+        }
+    }
+
+    cmd = Command::Get("hello2".to_string());
+    buf = cmd.to_bytes();
+    cmd = Command::from_bytes(&buf);
+    match cmd {
+        Command::Get(k) => {
+            assert_eq!(k, "hello2");
+        }
+        _ => {
+            panic!("test failed")
+        }
+    }
+
+    cmd = Command::Remove("hello3".to_string());
+    buf = cmd.to_bytes();
+    cmd = Command::from_bytes(&buf);
+    match cmd {
+        Command::Remove(k) => {
+            assert_eq!(k, "hello3");
+        }
+        _ => {
+            panic!("test failed")
+        }
     }
 }
--- src/log.rs
+++ src/log.rs
@@ -1,7 +1,9 @@
-use crate::cmd::Command;
 use crate::Result;
+use crate::cmd::Command;
+use anyhow::anyhow;
+use std::collections::HashMap;
 use std::fs::{File, OpenOptions};
-use std::io::{Seek, SeekFrom, Write};
+use std::io::{BufWriter, Read, Seek, SeekFrom, Write};
 use std::path::PathBuf;
 
 pub struct Position {
@@ -17,7 +19,7 @@ impl Position {
 
 pub struct Writer {
     id: u64,
-    file: File,
+    w: BufWriter<File>,
     pos: usize,
 }
 
@@ -26,14 +28,21 @@ impl Writer {
         let path = log_file_path(dir, id);
         let file = OpenOptions::new().create_new(true).write(true).open(path)?;
 
-        Ok(Self { id, file, pos: 0 })
+        Ok(Self {
+            id,
+            w: BufWriter::new(file),
+            pos: 0,
+        })
     }
 
     pub fn write(&mut self, cmd: Command) -> Result<Position> {
         let pos = self.pos;
         let buf = cmd.to_bytes();
-        self.file.write(&buf)?;
-        self.pos += buf.len();
+        self.w.write(&buf.len().to_be_bytes())?;
+        self.w.write(&buf)?;
+        self.w.flush()?;
+
+        self.pos += buf.len() + 8;
 
         let pos = Position::new(self.id, pos);
         Ok(pos)
@@ -43,6 +52,7 @@ impl Writer {
 pub struct Reader {
     id: u64,
     file: File,
+    pos: usize,
 }
 
 impl Reader {
@@ -50,14 +60,66 @@ impl Reader {
         let path = log_file_path(dir, id);
         let file = OpenOptions::new().read(true).open(path)?;
 
-        Ok(Self { id, file })
+        Ok(Self { id, file, pos: 0 })
     }
 
     pub fn read(&mut self, pos: usize) -> Result<Option<Command>> {
-        self.file.seek(SeekFrom::Start(pos as u64))?;
+        self.seek(pos)?;
+        self.read_at_current()
+    }
 
+    fn seek(&mut self, pos: usize) -> Result<()> {
+        if self.pos != pos {
+            self.file.seek(SeekFrom::Start(pos as u64))?;
+            self.pos = pos;
+        }
 
-        todo!()
+        Ok(())
+    }
+
+    fn read_at_current(&mut self) -> Result<Option<Command>> {
+        let mut len_buf = [0u8; 8];
+        let size = self.file.read(&mut len_buf)?;
+        if size == 0 {
+            //EOF
+            return Ok(None);
+        } else if size < len_buf.len() {
+            return Err(anyhow!("invalid file format"));
+        }
+
+        let len = u64::from_be_bytes(len_buf);
+        let mut buf = vec![0u8; len as usize];
+        self.file.read_exact(&mut buf)?;
+
+        self.pos += 8 + buf.len();
+
+        Ok(Some(Command::from_bytes(&buf)))
+    }
+
+    pub fn load(&mut self, data: &mut HashMap<String, Position>) -> Result<()> {
+        self.seek(0)?;
+
+        loop {
+            let pos = self.pos;
+            let opt = self.read_at_current()?;
+            match opt {
+                Some(cmd) => match cmd {
+                    Command::Set(key, value) => {
+                        let pos = Position::new(self.id, pos);
+                        data.insert(key, pos);
+                    }
+                    Command::Remove(key) => {
+                        data.remove(&key);
+                    }
+                    _ => {}
+                },
+                None => {
+                    break;
+                }
+            }
+        }
+
+        Ok(())
     }
 }
 
